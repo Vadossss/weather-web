0c9b5843069933bb59ec2e35aecb2ba7481a98a3
Vadossss <iv.gav789@mail.ru>
2026-03-02 10:20:28 +0300

    Отформотирован store

--- weather-frontend/src/app/store.ts
+++ weather-frontend/src/app/store.ts
@@ -1,40 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import favoritesReducer, {
-  type FavoritesState,
-} from "../features/favorites/favoritesSlice";
-
-// const loadFavoritesFromStorage = (): FavoritesState | undefined => {
-//   try {
-//     const data = localStorage.getItem("favorites");
-//     return data ? JSON.parse(data) : undefined;
-//   } catch (error) {
-//     console.error("Ошибка чтения localStorage:", error);
-//     return undefined;
-//   }
-// };
-
-// const preloadedState = {
-//   favorites: loadFavoritesFromStorage() || {
-//     items: [],
-//     totalItems: 0,
-//   },
-// };
+import favoritesReducer from "../features/favorites/favoritesSlice";
 
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
   },
-  // preloadedState,
 });
-
-// store.subscribe(() => {
-//   try {
-//     const state = store.getState();
-//     localStorage.setItem("favorites", JSON.stringify(state.favorites));
-//   } catch (error) {
-//     console.error("Ошибка сохранения localStorage:", error);
-//   }
-// });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
